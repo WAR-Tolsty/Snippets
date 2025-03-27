65a4728ab21961c785473605e96fdb343b5c07e8
Mikhail Stukalov <ya@mstukalov.ru>
2025-03-27 17:44:58 +0300

    Add login and logout

--- Snippets/urls.py
+++ Snippets/urls.py
@@ -3,6 +3,7 @@ from django.conf.urls.static import static
 from django.conf import settings
 from MainApp import views
 
+
 urlpatterns = [
     path('', views.index_page, name='home'),
     path('snippets/add', views.add_snippet_page, name='snippets-add'),
@@ -10,4 +11,6 @@ urlpatterns = [
     path('snippets/<int:snippet_id>', views.snippet_detail, name='snippet-detail'),
     path('snippets/<int:snippet_id>/edit', views.snippet_edit, name='snippet-edit'),
     path('snippets/<int:snippet_id>/delete', views.snippet_delete, name='snippet-delete'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout')
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
